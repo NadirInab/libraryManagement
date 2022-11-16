3c6a268ae8cf40bf51a452a2edbca89b3763c49e
nadir inab <nadir.inab.dev@gmail.com>
2022-11-16 14:46:42 +0100

    admin page is well set

--- templates/adminPage.php
+++ templates/adminPage.php
@@ -8,6 +8,7 @@
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia, cupiditate!</p>
         <img style="width: 100px ; height= 100px ;border-radius: 50%;" src="../images/<?= $_SESSION["profile"] ?>" alt="" > <br>
         <strong> <?= $_SESSION["phone"] ?></strong>
+        <p></p>
         <h1> <?= $_SESSION["id"] ?></h1>
     </div>
 <?php
