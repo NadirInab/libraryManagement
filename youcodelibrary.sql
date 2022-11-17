338c36422c613ce17514fd019e232595021d38bc
nadir inab <nadir.inab.dev@gmail.com>
2022-11-17 20:57:57 +0100

    db is added

--- youcodelibrary.sql
+++ youcodelibrary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 17 nov. 2022 à 20:55
+-- Généré le : jeu. 17 nov. 2022 à 20:56
 -- Version du serveur : 10.4.25-MariaDB
 -- Version de PHP : 8.1.10
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `youcodelibrary`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(10) NOT NULL,
+  `fullName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `pwd` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `fullName`, `email`, `image`, `phone`, `pwd`) VALUES
+(2, 'Nadir', 'nadir@gmail.com', 'user.png', '06 45 67 89 09', 'nadir'),
+(7, 'jomabymuk@mailinator.com', 'fupuj@mailinator.com', '', '+1 (985) 446-8673', 'Pa$$w0rd!'),
+(8, 'MEHDI', 'Mehdi@gmail.com', 'user.png', '06 45 67 89 09', 'rain');
 
 -- --------------------------------------------------------
 
@@ -42,11 +66,27 @@ CREATE TABLE `book` (
 --
 
 --
+-- Index pour la table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
 -- Index pour la table `book`
 --
 ALTER TABLE `book`
   ADD PRIMARY KEY (`isbn`),
   ADD KEY `admin_id` (`admin_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Contraintes pour les tables déchargées
